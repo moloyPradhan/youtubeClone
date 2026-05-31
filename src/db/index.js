ddf4d1c4972ea411f7cd67fd829004dac762363f
Moloy Pradhan <pradhanmoloy15@gmail.com>
2026-05-31 20:09:05 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,9 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        // const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`mongodb://admin:qwerty@mongo:27017/youtubeClone`)
         console.log(`\MongoDB connected !! DB Host : ${connectionInstance.connection.host}`);
-
 
     } catch (error) {
         console.log("MongoDB connection error : ", error);
